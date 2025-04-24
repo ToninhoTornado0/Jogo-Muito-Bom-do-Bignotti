82a73654a357176be5291527419db07eb0aa3326
GABRIEL BIGNOTTI <gabriel_bignotti@estudante.sesisenai.org.br>
2025-04-24 08:37:00 -0300

    Criação das classes Guerreiro, Mago e Arqueiro herdando de Personagem

--- script.js
+++ script.js
@@ -2,6 +2,26 @@ class personagem{
     constructor(vida ,força, mana, ){
         this.força = força;
         this.vida = vida;
-        this.mana = mana
+        this.mana = mana;
+    }
+}
+class personagem extends mago  {
+    constructor(bolaDeFogo){
+        super(força,vida,mana);
+        this.bolaDeFogo = bolaDeFogo;
+    }
+}
+
+class personagem extends guerreiro  {
+    constructor(defender){
+        super(força,vida,mana);
+        this.defender = defender;
+    }
+}
+
+class personagem extends arqueiro  {
+    constructor(atirar){
+        super(força,vida,mana);
+        this.atirar = atirar;
     }
 }
